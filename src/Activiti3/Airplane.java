1906190fd176830a9a7e9a1ba7ee975bd43ddf88
Kuznetsova-EV <steshani@mail.ru>
2025-10-06 03:17:39 +0400

    Продолжение работы

--- src/Activiti3/Airplane.java
+++ src/Activiti3/Airplane.java
@@ -10,18 +10,17 @@ public final class Airplane extends AirVehicle {
         this.passengerCapacity = passengerCapacity;
         this.wingspan = wingspan;
     }
-
     @Override
     public void getInfo() {
-        System.out.println("\n=== САМОЛЕТ ===");
+        System.out.println("\n САМОЛЕТ ");
         System.out.println("Марка: " + brand);
         System.out.println("Модель: " + model);
         System.out.println("Год: " + year);
-        System.out.println("Макс. высота: " + maxAltitude + " м");
+        System.out.println("Максимальная высота: " + maxAltitude + " м");
         System.out.println("Вместимость: " + passengerCapacity + " пассажиров");
         System.out.println("Размах крыльев: " + wingspan + " м");
         System.out.println("Двигатель: " + engine.getPower() + " л.с., " +
-                engine.getFuelType().getDescription());
+                engine.getFuel().getDescription());
     }
 
     public void deployLandingGear() {
