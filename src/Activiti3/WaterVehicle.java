b57bc707330218f7eb0931f39afe0807c19aff0b
Kuznetsova-EV <steshani@mail.ru>
2025-10-06 03:11:48 +0400

    Продолжение работы

--- src/Activiti3/WaterVehicle.java
+++ src/Activiti3/WaterVehicle.java
@@ -1,4 +1,24 @@
 package Activiti3;
 
-public class WaterVehicle {
+public sealed class WaterVehicle extends Transport permits Ship, Boat {
+    protected final int length;
+    protected final boolean hasSail;
+
+    public WaterVehicle(String brand, String model, int year, Engine engine, int length, boolean hasSail) {
+        super(brand, model, year, engine);
+        this.length = length;
+        this.hasSail = hasSail;
+    }
+
+    public void dropAnchor() {
+        System.out.println(brand + " " + model + " якорь в воду");
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public boolean hasSail() {
+        return hasSail;
+    }
 }
