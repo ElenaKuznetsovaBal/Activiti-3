b21175de2fd392c2d3acfa938532ba4f71296f8c
Kuznetsova-EV <steshani@mail.ru>
2025-10-06 03:16:04 +0400

    Продолжение работы

--- src/Activiti3/Bicycle.java
+++ src/Activiti3/Bicycle.java
@@ -10,25 +10,23 @@ public final class Bicycle extends LandVehicle {
 
     @Override
     public void startEngine() {
-        System.out.println("Велосипед не имеет двигателя - начинайте крутить педали!");
+        System.out.println("Крути педали!");
     }
 
     @Override
     public void stopEngine() {
-        System.out.println("Велосипед не имеет двигателя - прекратите крутить педали!");
+        System.out.println("Прекрати крутить педали!");
     }
-
     @Override
     public void getInfo() {
-        System.out.println("\n=== ВЕЛОСИПЕД ===");
+        System.out.println("\n ВЕЛОСИПЕД ");
         System.out.println("Марка: " + brand);
         System.out.println("Модель: " + model);
         System.out.println("Год: " + year);
-        System.out.println("Кол-во колес: " + wheelCount);
+        System.out.println("Количество колес: " + wheelCount);
         System.out.println("Наличие передач: " + (hasGears ? "Да" : "Нет"));
-        System.out.println("Тип привода: " + FuelType.HUMAN_POWER.getDescription());
+        System.out.println("Тип привода: " + Fuel.HORSE_POWER.getDescription());
     }
-
     public void ringBell() {
         System.out.println("Дзинь-дзинь! Звонок велосипеда");
     }
