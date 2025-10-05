6fd7844c3f3aba9f8159b135946ca6b725d54c0e
Kuznetsova-EV <steshani@mail.ru>
2025-10-06 03:22:47 +0400

    Продолжение работы

--- src/Activiti3/Boat.java
+++ src/Activiti3/Boat.java
@@ -1,4 +1,30 @@
 package Activiti3;
 
-public class Boat {
+public final class Boat extends WaterVehicle {
+    private final int passengerCapacity;
+
+    public Boat(String brand, String model, int year, Engine engine, int length, boolean hasSail, int passengerCapacity) {
+        super(brand, model, year, engine, length, hasSail);
+        this.passengerCapacity = passengerCapacity;
+    }
+
+    @Override
+    public void getInfo() {
+        System.out.println("\n=== ЛОДКА ===");
+        System.out.println("Марка: " + brand);
+        System.out.println("Модель: " + model);
+        System.out.println("Год: " + year);
+        System.out.println("Длина: " + length + " м");
+        System.out.println("Парус: " + (hasSail ? "Да" : "Нет"));
+        System.out.println("Вместимость: " + passengerCapacity + " человек");
+        if (engine != null) {
+            System.out.println("Двигатель: " + engine.getPower() + " л.с., " +
+                    engine.getFuelType().getDescription());
+        }
+    }
+
+    public void row() {
+        System.out.println("Лодка плывет на веслах");
+    }
 }
+
